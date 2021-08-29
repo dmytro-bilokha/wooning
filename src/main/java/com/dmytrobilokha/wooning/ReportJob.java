b17e2b7e867564b5adb5824283b9d8ab65752190
Dmytro Bilokha <dmytro@posteo.net>
2021-08-29 19:55:44 +0200

    Started the project with implementing HelloWorld

---
+++ src/main/java/com/dmytrobilokha/wooning/ReportJob.java
@@ -0,0 +1,19 @@
+package com.dmytrobilokha.wooning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReportJob {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportJob.class);
+
+    private ReportJob() {
+        //No instantiation
+    }
+
+    public static void main(String[] args) {
+        LOG.info("Hello world!");
+        return;
+    }
+
+}
